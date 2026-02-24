2fb00abcd09638eb2562d5d2532b856c72e3cd4e
Taras <nedovizt26@gmail.com>
2026-02-24 16:30:33 +0200

    Змінив код а також додав тести

--- lab2.py
+++ lab2.py
@@ -1,22 +1,29 @@
-def can_fit(side, w, h, n):
-    return (side // w) * (side // h) >= n
+def main(n, w, h):
+    def can_fit(side):
+        return (side // w) * (side // h) >= n
 
-def main():
-    n, w, h = map(int, input().split())
-
-    left = 0
-    right = 1
-
-    while not can_fit(right, w, h, n):
-        right *= 2
+    left = min(w, h)
+    right = n * max(w, h)
+    iterations = 0 
 
     while left + 1 < right:
         mid = (left + right) // 2
-        if can_fit(mid, w, h, n):
+        
+        if can_fit(mid):
             right = mid
         else:
             left = mid
+        iterations += 1
 
-    print(right)
+    print(f"Алгоритм впорався всього за {iterations} ітерацій!")
+    return right
 
-main()
+
+if __name__ == "__main__":
+    n = int(input("Введіть кількість листків (N): "))
+    w = int(input("Введіть ширину листка (W): "))
+    h = int(input("Введіть висоту листка (H): "))
+    
+    відповідь = main(n, w, h)
+    
+    print(f"Мінімальний розмір квадратної дошки: {відповідь} на {відповідь}")
